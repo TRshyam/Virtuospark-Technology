236cb05046b587504614fdac03780a6f65512605
dhanush0505 <dhanushviki100@gmail.com>
2024-02-06 21:08:50 +0530

    subscribe

--- assets/js/main.js
+++ assets/js/main.js
@@ -306,3 +306,19 @@
   // });
 
 })();
+function PopUp()
+{
+  $('subscribe').click(function(){
+    $('.alert').addClass("show");
+    $('.alert').removeClass("hide");
+    $('.alert').addClass("showAlert");
+    setTimeout(function(){
+      $('.alert').removeClass("show");
+      $('.alert').addClass("hide");
+    },5000);
+  });
+  $('.close-btn').click(function(){
+    $('.alert').removeClass("show");
+    $('.alert').addClass("hide");
+  });
+}
